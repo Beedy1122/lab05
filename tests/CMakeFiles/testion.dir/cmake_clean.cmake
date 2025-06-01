769adc62b76a8b7230409ad4317d32431e56e7c1
Beedy1122 <sashabeedy@gmail.com>
2025-06-01 20:55:09 +0300

    works

---
+++ tests/CMakeFiles/testion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testion.dir/test1.cpp.o"
+  "CMakeFiles/testion.dir/test1.cpp.o.d"
+  "testion"
+  "testion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/projects/lab05/tests
+# Build directory: /home/alex/projects/lab05/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests_lab5 "testion")
+set_tests_properties(tests_lab5 PROPERTIES  _BACKTRACE_TRIPLES "/home/alex/projects/lab05/tests/CMakeLists.txt;12;add_test;/home/alex/projects/lab05/tests/CMakeLists.txt;0;")
